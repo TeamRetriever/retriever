701254b5e13026893d08ba63eba5a928809b3732
ryan foley <ryandoesdata@gmail.com>
2025-11-12 10:56:06 -0500

    added public and private subnets to terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,6 +11,42 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_vpc" "example" {
+resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public-1a" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Public-1a"
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public-1a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_subnet" "private-1a" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "Private-1a"
+  }
 }
