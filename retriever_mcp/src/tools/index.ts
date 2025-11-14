2247ebbad09127abf1e676f0bb653c6e1a9831a5
Benjamin Walker <benjamin.q.walker@gmail.com>
2025-11-14 10:57:50 -0600

    Move `jaeger_api_v3_swagger.json` to `Development` directory for better organization.
    Co-authored-by: Zane Lee <zanelee28@yahoo.com>

--- retriever_mcp/src/tools/index.ts
+++ retriever_mcp/src/tools/index.ts
@@ -1,3 +1,4 @@
 export { listServicesTool } from "./listServices.js";
 export { getErrorsTool } from "./getErrors.js";
 export { findTraceTool } from "./findTraces.js";
+export { getErrorsTool } from "./getErrors.js";
