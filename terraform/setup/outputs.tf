77ad56ea31549b4cc5ee3e063d9ae40148fe6409
Benjamin Walker <benjamin.q.walker@gmail.com>
2025-12-02 08:12:20 -0600

    Export VPC and subnet IDs as Terraform outputs in `setup` module.
    Co-authored-by: Philip Knapp <philipsknapp@gmail.com>

---
+++ terraform/setup/outputs.tf
@@ -0,0 +1,14 @@
+output "vpc_id" {
+  description = "The id of the VPC created in Setup"
+  value = aws_vpc.main.id
+}
+
+output "public_subnet_id_a" {
+  description = "The id of public subnet A"
+  value = aws_subnet.public-a.id
+}
+
+output "private_subnet_id_b" {
+  description = "The id of public subnet B"
+  value = aws_subnet.public-b.id
+}
